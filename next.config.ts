36025a2d5c89d600cffed6102178f1d51ac8db3d
dhfai <105841108621@student.unismuh.ac.id>
2024-10-26 14:46:49 +0800

    solved base path

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/dashboard',
+            permanent: true, // Set to true for a 308 redirect, which is permanent
+          },
+        ];
+      },
     images: {
         domains: ["avatar.iran.liara.run"],
     },
